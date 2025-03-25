9742531c6c1ba127d8be3ae400cc2edcff24e01f
adelin <vatasescuadelin@gmail.com>
2025-03-25 18:55:02 +0100

    add reducer and logic for basket state, formatting

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,108 +1,216 @@
-import React, { useCallback, useEffect, useState } from "react";
-import { StyleSheet, FlatList, TouchableOpacity, Text } from "react-native";
+import React, { useReducer, useState } from "react";
+import {
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Text,
+  View,
+} from "react-native";
 
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { generateId, getTotalPrice } from "@/utils/general";
+import { Basket, Product } from "@/types/appTypes";
+import { useCreateOrder } from "@/hooks/useCreateOrder";
+import { usePayOrder } from "@/hooks/usePayOrder";
 
-const AUTH_USER_TOKEN = process.env.EXPO_PUBLIC_AUTH_USER_TOKEN ?? "";
+type BasketStateReducerAction =
+  | {
+      type: "add";
+      payload: Product;
+    }
+  | {
+      type: "remove";
+      payload: string; // id of product to remove
+    };
+const basketStateReducer = (
+  state: Basket,
+  action: BasketStateReducerAction
+) => {
+  switch (action.type) {
+    case "add": {
+      if (!action.payload) {
+        return state;
+      }
 
-type Product = {
-  id: string;
-  name: string;
-  price_unit: number;
-  vat_rate: number;
+      const productToAddId = action.payload.id;
+
+      const updatedCurrentProducts = state.products.map((currentProduct) => {
+        if (currentProduct.id === productToAddId) {
+          return { ...currentProduct, quantity: currentProduct.quantity + 1 };
+        } else {
+          return { ...currentProduct };
+        }
+      });
+
+      const productIsAlreadyInBasket = state.products.some(
+        (existingProduct) => existingProduct.id === productToAddId
+      );
+      // const isProductAlreadyInBasket =
+      // const existingProducts = state.products
+      //   .filter((currentProduct) => currentProduct.id === productToAddId)
+      //   .map((product) => ({ ...product, quantity: product.quantity + 1 }));
+
+      // state.products.map((currentProduct) => {
+      //   // when product is already in basket
+      //   if (productToAddId === currentProduct.id) {
+      //     return { ...currentProduct, quantity: currentProduct.quantity + 1 };
+      //   }
+      // });
+
+      return {
+        ...state,
+        products: productIsAlreadyInBasket
+          ? [...updatedCurrentProducts]
+          : [...updatedCurrentProducts, { ...action.payload, quantity: 1 }],
+        // products: [...existingProducts, { ...action.payload, quantity: 1 }],
+      };
+    }
+    case "remove": {
+      if (!action.payload) {
+        return { ...state };
+      }
+
+      return {
+        ...state,
+        products: state.products.filter(
+          (currentProduct) => currentProduct.id !== action.payload
+        ),
+      };
+    }
+    default:
+      return state;
+  }
 };
 
+const mockProducts: Product[] = [
+  {
+    id: "1",
+    name: "First",
+    price_unit: 1,
+    vat_rate: 1,
+  },
+  {
+    id: "2",
+    name: "Second",
+    price_unit: 2,
+    vat_rate: 1,
+  },
+];
+
+const initialBasketState: Basket = {
+  id: generateId(),
+  products: [],
+};
 export default function PosScreen() {
-  const [basket, setBasket] = useState([]);
-  const [products, setProducts] = useState([] as Product[]);
+  const [basket, dispatch] = useReducer(basketStateReducer, initialBasketState);
+  const [products, setProducts] = useState(mockProducts);
   const [orderId, setOrderId] = useState<string | null>(null);
 
-  useEffect(() => {
-    fetch("https://kanpla-code-challenge.up.railway.app/products", {
-      headers: {
-        "x-auth-user": AUTH_USER_TOKEN,
-      },
-    })
-      .then((response) => response.json())
-      .then((json) => setProducts(json))
-      .catch((error) => console.error(error));
-  }, []);
+  const { createOrder } = useCreateOrder();
+  const { payOrder } = usePayOrder();
 
-  const renderProduct = ({ item }) => (
-    <TouchableOpacity
-      style={styles.product}
-      onPress={() => setBasket((prev) => [...prev, item])}
-    >
-      <Text style={styles.text}>{item.name}</Text>
-      <Text style={styles.text}>${item.price_unit * (item.vat_rate + 1)}</Text>
-    </TouchableOpacity>
-  );
+  const totalPrice = getTotalPrice(basket);
 
-  const createOrder = () => {
-    fetch("https://kanpla-code-challenge.up.railway.app/orders", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "x-auth-user": AUTH_USER_TOKEN,
-      },
-      body: JSON.stringify({
-        total: basket.reduce((acc, item) => acc + item.price_unit, 0),
-      }),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        setOrderId(json.id);
-      })
-      .catch((error) => console.error(error));
+  const handleOnPressCreateOrder = async () => {
+    await createOrder({
+      total: totalPrice,
+      order_id: generateId(),
+      basket_id: basket.id,
+    });
   };
+  const handleOnPressPayOrder = async () => {};
 
-  const payOrder = useCallback(() => {
-    fetch(`https://kanpla-code-challenge.up.railway.app/payments`, {
-      method: "POST",
-      headers: {
-        "x-auth-user": AUTH_USER_TOKEN!,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        order_id: orderId,
-        amount: basket.reduce((acc, item) => acc + item.price_unit, 0),
-      }),
-    })
-      .then((response) =>
-        response.status === 201 ? response.json() : Promise.reject(response)
-      )
-      .then((json) => {
-        fetch(
-          `https://kanpla-code-challenge.up.railway.app/orders/${json.order_id}`,
-          {
-            method: "PATCH",
-            headers: {
-              "x-auth-user": AUTH_USER_TOKEN,
-            },
-            body: JSON.stringify({
-              status: "completed",
-            }),
-          }
-        )
-          .then((response) =>
-            response.status === 201 ? response.json() : Promise.reject(response)
-          )
-          .then((json) => {
-            setBasket([]);
-            setOrderId(null);
-          })
-          .catch((error) => console.error(error));
-      })
-      .catch((error) => console.error(error));
-  }, [orderId, basket]);
+  const handleOnPressRemoveProduct = (productId: string) => {
+    dispatch({ type: "remove", payload: productId });
+  };
+  // useEffect(() => {
+  //   fetch("https://kanpla-code-challenge.up.railway.app/products", {
+  //     headers: {
+  //       "x-auth-user": AUTH_USER_TOKEN,
+  //     },
+  //   })
+  //     .then((response) => response.json())
+  //     .then((json) => setProducts(json))
+  //     .catch((error) => console.error(error));
+  // }, []);
+
+  // const createOrder = () => {
+  //   fetch("https://kanpla-code-challenge.up.railway.app/orders", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       "x-auth-user": AUTH_USER_TOKEN,
+  //     },
+  //     body: JSON.stringify({
+  //       total: basket.reduce((acc, item) => acc + item.product.price_unit, 0),
+  //     }),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       setOrderId(json.id);
+  //     })
+  //     .catch((error) => console.error(error));
+  // };
+
+  // const payOrder = useCallback(() => {
+  //   fetch(`https://kanpla-code-challenge.up.railway.app/payments`, {
+  //     method: "POST",
+  //     headers: {
+  //       "x-auth-user": AUTH_USER_TOKEN!,
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       order_id: orderId,
+  //       amount: basket.reduce((acc, item) => acc + item.product.price_unit, 0),
+  //     }),
+  //   })
+  //     .then((response) =>
+  //       response.status === 201 ? response.json() : Promise.reject(response)
+  //     )
+  //     .then((json) => {
+  //       fetch(
+  //         `https://kanpla-code-challenge.up.railway.app/orders/${json.order_id}`,
+  //         {
+  //           method: "PATCH",
+  //           headers: {
+  //             "x-auth-user": AUTH_USER_TOKEN,
+  //           },
+  //           body: JSON.stringify({
+  //             status: "completed",
+  //           }),
+  //         }
+  //       )
+  //         .then((response) =>
+  //           response.status === 201 ? response.json() : Promise.reject(response)
+  //         )
+  //         .then((json) => {
+  //           setBasket([]);
+  //           setOrderId(null);
+  //         })
+  //         .catch((error) => console.error(error));
+  //     })
+  //     .catch((error) => console.error(error));
+  // }, [orderId, basket]);
+
+  console.log("basket: ", basket);
 
   return (
     <ThemedView style={styles.container}>
       <ThemedView style={styles.productGrid}>
         <FlatList
           data={products}
-          renderItem={renderProduct}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.product}
+              onPress={() => dispatch({ type: "add", payload: item })}
+            >
+              <Text style={styles.text}>{item.name}</Text>
+              <Text style={styles.text}>
+                ${item.price_unit * item.vat_rate}
+              </Text>
+            </TouchableOpacity>
+          )}
           keyExtractor={(item) => item.id}
           numColumns={2}
         />
@@ -113,24 +221,31 @@ export default function PosScreen() {
           Basket
         </ThemedText>
 
-        {basket.map((item, index) => (
-          <ThemedView key={index} style={styles.basketItem}>
-            <Text style={styles.text}>{item.name}</Text>
-            <Text style={styles.text}>${item.price}</Text>
-          </ThemedView>
+        {basket?.products?.map((item, index) => (
+          <View key={index} style={styles.basketItem}>
+            <TouchableOpacity
+              onPress={() => handleOnPressRemoveProduct(item.id)}
+            >
+              <Text style={styles.text}>{item.name}</Text>
+              <Text style={styles.text}>
+                ${item.price_unit * item.vat_rate}
+              </Text>
+            </TouchableOpacity>
+          </View>
         ))}
 
-        <ThemedText style={styles.text}>
-          Total: ${basket.reduce((acc, item) => acc + item.price_unit, 0)}
-        </ThemedText>
+        <ThemedText style={styles.text}>Total: ${totalPrice}</ThemedText>
 
-        <TouchableOpacity style={styles.button} onPress={createOrder}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleOnPressCreateOrder}
+        >
           <ThemedText style={styles.buttonText}>Create Order</ThemedText>
         </TouchableOpacity>
 
         <TouchableOpacity
           style={[styles.button, !orderId && { backgroundColor: "#555" }]}
-          onPress={payOrder}
+          onPress={handleOnPressPayOrder}
           disabled={!orderId}
         >
           <ThemedText style={styles.buttonText}>Pay</ThemedText>
